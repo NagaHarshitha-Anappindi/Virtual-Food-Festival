82e655602b189a1d8fd143d460ae49dcc80ef07e
Gayatri <gayatrivvns@gmail.com>
2025-03-11 20:57:27 +0530

    dashboard is added

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css"; // Add the `./` to indicate it's in the same folder
+
+
+const Dashboard = () => {
+  return (
+    <div className="dashboard-container">
+      <h1>Welcome to the Dashboard</h1>
+      
+    </div>
+  );
+};
+
+export default Dashboard;
